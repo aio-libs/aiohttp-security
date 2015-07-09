7ab8a7220ebc405404a5e27d9943b17cd1108834
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-07-09 07:27:25 +0300

    Fix pep8 error

--- aiohttp_security/__init__.py
+++ aiohttp_security/__init__.py
@@ -1,8 +1,8 @@
-__version__ = '0.1.0'
-
-
 from .abc import AbstractIdentityPolicy, AbstractAuthorizationPolicy
 from .api import remember, forget, setup, authorized_userid, permits
+
+
+__version__ = '0.1.0'
 
 
 __all__ = ('AbstractIdentityPolicy', 'AbstractAuthorizationPolicy',
