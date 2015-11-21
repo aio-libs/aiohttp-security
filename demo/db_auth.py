fd58337780f3a922d5de103f457b2601207776e7
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-11-21 09:37:44 +0200

    work on demo

--- demo/db_auth.py
+++ demo/db_auth.py
@@ -1,6 +1,9 @@
 import asyncio
+import sqlalchemy as sa
 
 from aiohttp_security.authorization import AbstractAuthorizationPolicy
+
+from . import db
 
 
 class DBAuthorizationPolicy(AbstractAuthorizationPolicy):
@@ -8,16 +11,25 @@ class DBAuthorizationPolicy(AbstractAuthorizationPolicy):
         self.db_pool = db_pool
 
     @asyncio.coroutine
+    def authorized_user_id(self, identity):
+        with (yield from self.db_pool) as conn:
+            where = [db.users.c.login == identity,
+                     not db.users.c.disabled]
+            query = db.users.count().where(sa.and_(*where))
+            ret = yield from conn.scalar(query)
+            if ret:
+                return identity
+            else:
+                return None
+
+    @asyncio.coroutine
     def permits(self, identity, permission, context=None):
+        with (yield from self.db_pool) as conn:
+            where = [db.users.c.login == identity,
+                     not db.users.c.disabled]
         record = self.data.get(identity)
         if record is not None:
             # TODO: implement actual permission checker
             if permission in record:
                 return True
         return False
-
-    @asyncio.coroutine
-    def authorized_user_id(self, identity):
-        with (yield from self.db_pool) as conn:
-            conn
-        return identity if identity in self.data else None
