0cfd9889b9764e559f982ff019aca30f00fe00ed
Colin Watson <cjwatson@debian.org>
2024-12-19 15:02:32 +0000

    Port to Sphinx 8.0 (#832)

--- docs/conf.py
+++ docs/conf.py
@@ -330,5 +330,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3': None,
-                       'http://aiohttp.readthedocs.org/en/stable': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'aiohttp': ('http://aiohttp.readthedocs.org/en/stable', None),
+}
