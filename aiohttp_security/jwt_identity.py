9b66e29e09bf441cca31a80b72a04691603e2201
maillol <vmaillol@e.email>
2025-01-28 19:45:17 +0100

    Rename JWTIdentityError to InvalidAuthorizationScheme

--- aiohttp_security/jwt_identity.py
+++ aiohttp_security/jwt_identity.py
@@ -11,23 +11,20 @@ from .abc import AbstractIdentityPolicy
 try:
     import jwt
     HAS_JWT = True
+    _bases_error = (jwt.exceptions.PyJWTError, ValueError)
 except ImportError:  # pragma: no cover
     HAS_JWT = False
+    _bases_error = (ValueError, )
 
 
 AUTH_HEADER_NAME = 'Authorization'
 AUTH_SCHEME = 'Bearer '
 
 
-if HAS_JWT:
-    # This class inherits from ValueError to maintain backward compatibility
-    # with previous versions of aiohttp-security.
-    class JWTIdentityError(jwt.exceptions.PyJWTError, ValueError):
-        pass
-
-else:
-    class JWTIdentityError(ValueError):
-        pass
+# This class inherits from ValueError to maintain backward compatibility
+# with previous versions of aiohttp-security
+class InvalidAuthorizationScheme(*_bases_error):
+    pass
 
 
 class JWTIdentityPolicy(AbstractIdentityPolicy):
@@ -45,15 +42,14 @@ class JWTIdentityPolicy(AbstractIdentityPolicy):
             return None
 
         if not header_identity.startswith(AUTH_SCHEME):
-            raise JWTIdentityError("Invalid authorization scheme. "
-                                   + "Should be `{}<token>`".format(AUTH_SCHEME))
+            raise InvalidAuthorizationScheme("Invalid authorization scheme. "
+                                             "Should be `{}<token>`".format(AUTH_SCHEME))
 
         token = header_identity.split(' ')[1].strip()
 
         identity = jwt.decode(token,
                               self.secret,
                               algorithms=[self.algorithm])
-
         return identity.get(self.key)  # type: ignore[no-any-return]
 
     async def remember(self, request: web.Request, response: web.StreamResponse,
